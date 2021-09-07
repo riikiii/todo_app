365c507c467caee00c7b2476899172e8e2c707cf
riikiii <pumpkinkhayega@gmail.com>
2021-09-07 10:15:18 +0530

    first commit

---
+++ basic/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField 
+
+class Task(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.title 
+
+    class Meta:
+        ordering = ['complete'] 
+
+
